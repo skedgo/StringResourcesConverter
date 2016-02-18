6d0bf01d57ef075434e9043b567037c0bc703b73
Emmanuel Lagarrigue Lazarte <emmanuel.lagarrigue@black-tobacco.com>
2016-02-18 13:59:04 -0300

    Update samples

--- Sample/src/com/skedgo/tools/stringconvertersample/SampleUtils.java
+++ Sample/src/com/skedgo/tools/stringconvertersample/SampleUtils.java
@@ -5,27 +5,47 @@ import java.io.InputStream;
 import com.skedgo.tools.model.StringsStructure;
 import com.skedgo.tools.platform.android.AndroidOutputStrategy;
 import com.skedgo.tools.platform.ios.IOSInputStrategy;
+import com.skedgo.tools.platform.java.JavaPropertiesOutputStrategy;
 
 public class SampleUtils {
 
-	public void IOStoAndroidSample(){
-		
+	public void IOStoAndroidSample() {
+
 		InputStream input = this.getClass().getClassLoader().getResourceAsStream("assets/Localizable.strings");
-		
+
 		IOSInputStrategy inputStrategy = IOSInputStrategy.getInstance();
 		AndroidOutputStrategy outputStrategy = AndroidOutputStrategy.getInstance();
 
-		 try {
+		try {
 			StringsStructure structure = inputStrategy.getInputValues(input);
 			String output = outputStrategy.generateOutput(structure);
-			
+
 			System.out.println(output);
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
+	}
+
+	public void IOStoJavaPropertiesSample() {
+
+		InputStream input = this.getClass().getClassLoader().getResourceAsStream("assets/Localizable.strings");
+
+		IOSInputStrategy inputStrategy = IOSInputStrategy.getInstance();
+		JavaPropertiesOutputStrategy outputStrategy = JavaPropertiesOutputStrategy.getInstance();
+
+		try {
+			StringsStructure structure = inputStrategy.getInputValues(input);
+			String output = outputStrategy.generateOutput(structure);
+
+			System.out.println(output);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
